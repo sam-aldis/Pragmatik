3eaec0db47eee8f47efaa027198fddeac535e31c
Sam Aldis <samaldis@gmail.com>
2018-02-21 13:52:35 +0900

    Created Virtual Env
    Created a virtual environment for the Server project so anyone can download it and get the packages needed

--- Server/includes/database.py
+++ Server/includes/database.py
@@ -1,21 +1,27 @@
 import sqlite3
 
+DB_LOCATION = "./db/clients.db"
+
 def install():
-    con = sqlite3.connect("./db/clients.db")
-    results = con.cursor().execute("CREATE TABLE trackers (ip text, connected int)")
+    con = sqlite3.connect(DB_LOCATION)
+    con.cursor().execute("CREATE TABLE trackers (ip text, connected int)")
     con.commit()
     con.close()
 
 def get_clients():
-    con = sqlite3.connect("./db/clients.db")
+    con = sqlite3.connect(DB_LOCATION)
     try:
-        results = con.cursor().execute("SELECT * FROM trackers")
-        for res in results:
-            print(res)
+        results = con.cursor().execute("SELECT * FROM trackers WHERE connected > 0")
+        con.close()
+        return results
     except:
+        con.close()
         install()
-        get_clients()
-    con.close()
+        return get_clients()
+
+def get_client(ip):
+    con = sqlite3.connect(DB_LOCATION)
 
 def add_client(ip,active):
-    con = sqlite3
+    con = sqlite3.connect(DB_LOCATION)
+    
